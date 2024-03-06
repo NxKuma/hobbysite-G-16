0153e2ba39422a96e944c948563f0cd0553884e4
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-06 15:44:51 +0800

    Adjusted spacing

--- merchstore/models.py
+++ merchstore/models.py
@@ -8,6 +8,7 @@ class ProductType(models.Model):
     class Meta:
         ordering = ['name',]
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(
