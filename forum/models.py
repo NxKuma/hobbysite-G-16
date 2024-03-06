f90e23568902b08bc0c811aca49e452b4d04c623
NxKuma <neilisraelko01@gmail.com>
2024-03-06 15:37:20 +0800

    Added Forum App and Models

---
+++ forum/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from django.db import models
+
+class PostCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    class Meta:
+        ordering = ['name']    
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(
+        'PostCategory',
+        on_delete=models.SET_NULL,
+        related_name='post'
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) 
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']  
+    
