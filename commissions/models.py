a2750399ffcf25a53ff017bc37a2c86e5d118242
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-03-06 17:40:24 +0800

    Added commission and comment models

--- commissions/models.py
+++ commissions/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.urls import inverse
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('commissions:commission', args=[self.pk])
+
+    class Meta:
+        ordering = ['created_on']
+
+
+class Comment(models.Model):
+    commission = models.ForeignKey(
+        'Commission',
+        on_delete=models.CASCADE,
+        related_name='comment'
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    def get_absolute_url(self):
+        return reverse('commissions:comment', args=[self.pk])
+
+    class Meta:
+        ordering = ['-created_on']
