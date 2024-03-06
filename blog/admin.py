3300ee1807e4a1f253a706201e5259b738f3bde9
JohnverLavina <johnver.lavina@student.ateneo.edu>
2024-03-06 20:06:11 +0800

    added admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
-
 # Register your models here.
+from django.contrib import admin
+from .models import Article, ArticleCategory
+
+class ArticleInLine(admin.StackedInline):
+    model = ArticleCategory
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+    inlines = [ArticleInLine,]
+
+admin.site.register(Article, ArticleAdmin)
+
+
+
