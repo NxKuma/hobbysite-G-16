22bc2b60f7f313a71fcb73ca0f4fec9c974b6c9f
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-06 16:29:57 +0800

    Views of Product and ProductType in progress

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Product, ProductType
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product.html'
+
+
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = 'product_detail.html'
+
+
+# class ProductTypeListView(ListView):
+#     model = ProductType
+#     template_name = 'producttype.html'
+
+
+class ProductTypeDetailView(DetailView):
+    model = ProductType
+    template_name = 'producttype.html'
+    
