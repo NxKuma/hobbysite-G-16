0d904378be267962103ac9a9cd2ac300a59f295f
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-03-06 17:44:31 +0800

    Added commission list and commission detail views in views

--- commissions/views.py
+++ commissions/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import Commission
+
+class CommissionListView(ListView):
+	model = Commission
+	template_name = "commission_list.html"
+
+
+class CommissionDetailView(DetailView):
+	model = Commission
+	template_name = "commission_detail.html"
