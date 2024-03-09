676bee2e9b07ad0feef5b5bb7e8336f11e44fa36
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-09 16:02:40 +0800

    Modified views of models and renamed templates accordingly

--- merchstore/views.py
+++ merchstore/views.py
@@ -4,11 +4,11 @@ from django.views.generic.detail import DetailView
 from .models import Product, ProductType
 
 class ProductTypeListView(ListView):
-    model = Product
-    template_name = 'product_type_list.html'
+    model = ProductType
+    template_name = 'product_list.html'
 
 
 class ProductDetailView(DetailView):
-    model = ProductType
-    template_name = 'product.html'
+    model = Product
+    template_name = 'product_detail.html'
     
