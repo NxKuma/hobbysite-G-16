5ffe48b19622fde8ce7a48f781a76f5e8935a142
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-03-11 18:20:23 +0800

    Added inline for admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -3,12 +3,17 @@ from django.contrib import admin
 from .models import Commission, Comment
 
 
+class CommentInline(admin.StackedInline):
+    model = Comment
+
+
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
 
 
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
+    inlines = [CommentInline]
 
 
 admin.site.register(Commission, CommissionAdmin)
