3fed9fe0253a113617a95198df9ac529b00e877f
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-19 15:27:25 +0800

    Modified and finalized the models

--- merchstore/models.py
+++ merchstore/models.py
@@ -5,7 +5,7 @@ class ProductType(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-    def __self__(self):
+    def __str__(self):
         return self.name
     
     def get_absolute_url(self):
@@ -19,12 +19,14 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(
         'ProductType',
-        on_delete = models.CASCADE,
+        on_delete = models.SET_NULL,
+        related_name = 'products',
+        null=True,
     )
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=100)
 
-    def __self__(self):
+    def __str__(self):
         return self.name
     
     def get_absolute_url(self):
