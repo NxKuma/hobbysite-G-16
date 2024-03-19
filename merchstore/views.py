c32221e8b3d6c3b5f25d07f5b297b8ff01e993b7
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-19 15:29:35 +0800

    Modified and finalized views and templates

--- merchstore/views.py
+++ merchstore/views.py
@@ -3,9 +3,9 @@ from django.views.generic.detail import DetailView
 
 from .models import Product, ProductType
 
-class ProductTypeListView(ListView):
-    model = ProductType
-    template_name = 'product_list.html'
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products.html'
 
 
 class ProductDetailView(DetailView):
