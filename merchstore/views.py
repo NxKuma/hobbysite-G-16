d66df650f159f99f231ca79c75567cdbf0856bf4
larcomicious <marco.gimena@student.ateneo.edu>
2024-03-20 16:43:16 +0800

    Cleaned code to standard

--- merchstore/views.py
+++ merchstore/views.py
@@ -3,12 +3,13 @@ from django.views.generic.detail import DetailView
 
 from .models import Product, ProductType
 
+
 class ProductListView(ListView):
-    model = Product
-    template_name = 'products.html'
+    model = ProductType
+    template_name = 'merchstore-list.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'product_detail.html'
+    template_name = 'merchstore-detail.html'
     
