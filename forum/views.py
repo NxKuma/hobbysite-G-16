82b9f24c2188c281403394fadeaae21de7bbbe4a
NxKuma <neilisraelko01@gmail.com>
2024-03-20 17:35:23 +0800

    Updated Forum

--- forum/views.py
+++ forum/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Post
+from .models import Post, PostCategory
 
 
 class ThreadListView(ListView):
-    model = Post
+    model = PostCategory
     template_name = "forum-list.html"
 
 
