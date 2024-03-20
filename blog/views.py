b1815ef59a06c284673a63618db177fe493059ba
NxKuma <neilisraelko01@gmail.com>
2024-03-20 17:29:28 +0800

    Johnver working on Neil's machine: updated html files and views

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,14 @@
 # Create your views here.
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Article
+from .models import Article, ArticleCategory
 
 
 class ArticleListView(ListView):
-	model = Article  
-	template_name = "article-list.html"
+	model = ArticleCategory
+	template_name = "blog-list.html"
 
 class ArticleDetailView(DetailView):
 	model = Article
-	template_name = "article-detail.html"
+	template_name = "blog-detail.html"
