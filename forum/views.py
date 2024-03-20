f4c43a2cbcb0c9705c875323d34b7d67fac3df50
NxKuma <neilisraelko01@gmail.com>
2024-03-20 14:06:36 +0800

    Modified File Names

--- forum/views.py
+++ forum/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Post, PostCategory
+from .models import Post
 
 
 class ThreadListView(ListView):
     model = Post
-    template_name = "threads.html"
+    template_name = "forum-list.html"
 
 
 class ThreadDetailView(DetailView):
     model = Post
-    template_name = "thread.html"
+    template_name = "forum-detail.html"
