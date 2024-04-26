07051c7fbd208a49ac41c099583b288bf954fed7
NxKuma <neilisraelko01@gmail.com>
2024-04-26 16:18:09 +0800

    Added Static Folders

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +12,5 @@ urlpatterns = [
     path('wiki/', include('wiki.urls', namespace='wiki')),
     path('blog/', include('blog.urls', namespace='blog')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
