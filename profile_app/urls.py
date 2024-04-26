c5992df1fd3bd6f23a62987f3ee51ccd546b2761
NxKuma <neilisraelko01@gmail.com>
2024-04-26 14:25:40 +0800

    Added Other Parts of Profile App

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,1 +1,9 @@
+from django.urls import path
+
+from .views import ProfileUpdateView
+
+urlpatterns = [
+    path('profile', ProfileUpdateView.as_view(), name='profile-update')
+]
+
 app_name = 'profile'
