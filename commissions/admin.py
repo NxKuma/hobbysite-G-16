3857c344354a49d492178d8008930b29d251a329
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-05-01 18:16:30 +0800

    Changed Comment to Job in admin

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,20 +1,20 @@
 from django.contrib import admin
 
-from .models import Commission, Comment
+from .models import Commission, Job
 
 
-class CommentInline(admin.StackedInline):
-    model = Comment
+class JobInline(admin.StackedInline):
+    model = Job
 
 
-class CommentAdmin(admin.ModelAdmin):
-    model = Comment
+class JobAdmin(admin.ModelAdmin):
+    model = Job
 
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
-    inlines = [CommentInline]
+    inlines = [JobInline]
 
 
 admin.site.register(Commission, CommissionAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Job, JobAdmin)
