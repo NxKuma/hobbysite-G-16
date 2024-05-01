e07143c0012de2b9d7909bec3caa7821e22a5fdd
NxKuma <neilisraelko01@gmail.com>
2024-05-01 21:10:50 +0800

    Fixed Name in Create View #2 & Separated the Threads

--- forum/views.py
+++ forum/views.py
@@ -6,11 +6,19 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Thread, ThreadCategory
 from .forms import ThreadForm, CommentForm
+from user_management import models as ProfileModel
 
 
 class ThreadListView(ListView):
     model = ThreadCategory
     template_name = "forum-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        author = ProfileModel.Profile.objects.get(user=self.request.user)
+        threads_by_author = Thread.objects.filter(author=author)
+        context['threads_by_author'] = threads_by_author
+        return context
 
 
 class ThreadDetailView(LoginRequiredMixin, DetailView):
@@ -20,9 +28,8 @@ class ThreadDetailView(LoginRequiredMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        main_model = self.get_object()
-        category = main_model.category
-        threads_in_category = Thread.objects.filter(category=category)
+        thread = self.get_object()
+        threads_in_category = Thread.objects.filter(category=thread.category)
         context['threads_in_category'] = threads_in_category
         return context
 
@@ -39,12 +46,14 @@ class ThreadCreateView(LoginRequiredMixin, CreateView):
         })
     
     def form_valid(self, form):
-        form.instance.user = self.request.user  
+        form.instance.user = self.request.user
+        form.instance.display_name = form.cleaned_data['author']
         return super().form_valid(form)
     
     def get_initial(self):
         initial = super().get_initial()
-        initial['author'] = self.request.user  
+        if self.request.user.is_authenticated:
+            initial['author'] = self.request.user.id
         return initial
 
 
