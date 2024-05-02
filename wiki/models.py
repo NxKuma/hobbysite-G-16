742e9e164a724c890c1954136707d88e93db6bf4
NxKuma <neilisraelko01@gmail.com>
2024-05-02 22:11:47 +0800

    Added Appropriate Models

--- wiki/models.py
+++ wiki/models.py
@@ -3,6 +3,8 @@ from datetime import datetime
 from django.db import models
 from django.urls import reverse
 
+from user_management import models as ProfileModel
+
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -20,12 +22,18 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        ProfileModel.Profile,
+        on_delete = models.SET_NULL,
+        related_name='author'
+    )
     article_category = models.ForeignKey(
         'ArticleCategory',
         on_delete = models.SET_NULL,
         related_name='article',
         null = True
     )
+    header_image = models.ImageField(upload_to='images/')
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -38,3 +46,22 @@ class Article(models.Model):
 
     class Meta:
         ordering = ['-created_on',]
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        ProfileModel.Profile,
+        on_delete=models.SET_NULL,
+        related_name='comments'
+    )
+    article = models.ForeignKey(
+        'Article',
+        on_delete=models.CASCADE,
+        related_name='article_comments',
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True) 
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on',]
