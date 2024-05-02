3d51fdebc4a3a5a5b3caf7e88ed0ed4ef10c63a5
larcomicious <marco.gimena@student.ateneo.edu>
2024-05-02 18:29:00 +0800

    Implemented the Transaction form in DetailView

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
@@ -25,6 +26,27 @@ class ProductDetailView(DetailView):
     model = Product
     template_name = 'merchstore-detail.html'
 
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        product = self.get_object()
+        transaction_form = TransactionForm(initial={'product': product})
+
+        if product.owner == self.request.user:
+            return redirect('merchstore-list')
+
+        ctx['transaction_form'] = transaction_form
+        return ctx
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        transaction_form = TransactionForm(request.POST, initial={'product': self.object, 'buyer': self.request.user})
+        if transaction_form.is_valid():
+            transaction = transaction_form.save(commit=False)
+            transaction.buyer = ProfileModel.Profile.objects.get(user=self.request.user)
+            transaction.save()
+            return redirect('cart')
+        ctx = self.get_context_data(object=self.object, transaction_from=transaction_form )
+        return self.render_to_response(ctx)
 
 class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
@@ -59,6 +81,6 @@ class CartView(DetailView):
     template_name = 'merchstore-cart.html'
 
 
-class TransactionsListView(DetailView):
+class TransactionsListView(ListView):
     model = Transaction
     template_name = 'merchstore-transactions.html'
