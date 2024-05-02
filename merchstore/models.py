dc59d3cadf8014a2ad1aec00f1c0f20b9e6757bd
larcomicious <marco.gimena@student.ateneo.edu>
2024-05-02 16:48:26 +0800

    Updated models of Merchstore

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+from user_management import models as ProfileModels
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -22,11 +23,30 @@ class Product(models.Model):
         'ProductType',
         on_delete = models.SET_NULL,
         related_name = 'products',
-        null=True,
+        null = True,
     )
+    owner = models.ForeignKey(
+        ProfileModels.Profile,
+        on_delete = models.CASCADE,
+        related_name = 'products'
+    )
+    
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=100)
+    stock = models.PositiveIntegerField()
 
+    class States(models.TextChoices):
+        # Actual value ↓      # ↓ Displayed on Django Admin
+        AVAILABLE = 'Available', 'Available'
+        ON_SALE = 'On sale', 'On sale'
+        OUT_OF_STOCK = 'Out of stock', 'Out of stock'
+    
+    status = models.CharField(
+        max_length = 12,
+        choices = States,
+        default = States.AVAILABLE
+    )
+    
     def __str__(self):
         return self.name
     
@@ -35,3 +55,30 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['name',]
+
+class Transaction(models.Model):
+    buyer = models.ForeignKey(
+        ProfileModels.Profile,
+        on_delete = models.SET_NULL,
+        null = True,
+        related_name = 'transactions'
+    )
+    product = models.ForeignKey(
+        'Product',
+        on_delete = models.SET_NULL,
+        null = True,
+        related_name = 'transactions'
+    )
+    amount = models.PositiveIntegerField()
+
+    class States(models.TextChoices):
+        ONCART = 'On cart', 'On cart'
+        TOPAY = 'To pay', 'To pay'
+        TOSHIP = 'To ship', 'To ship'
+        TORECEIVE = 'To receive', 'To receive'
+        DELIVERED = 'Delivered', 'Delivered'
+    
+    status = models.CharField(
+        max_length = 10,
+        choices = States)
+    created_on = models.DateTimeField(auto_now_add=True)
