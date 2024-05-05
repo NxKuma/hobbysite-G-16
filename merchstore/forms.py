c712d2b03976545ba5aded9975f82b0d680f7590
larcomicious <marco.gimena@student.ateneo.edu>
2024-05-05 17:36:55 +0800

    Cleaned up code

--- merchstore/forms.py
+++ merchstore/forms.py
@@ -10,31 +10,9 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
     
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['owner'].disabled = True
 
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ('amount', 'status')
-
-    # def save(self, commit=True):
-    #     transaction = super().save(commit=False)
-    #     if commit:
-    #         transaction.save()
-
-    #         # Check if user is authenticated
-    #         if self.user.is_authenticated:
-    #             # Redirect to CartView
-    #             return HttpResponseRedirect(reverse('merchstore:cart'))
-    #         else:
-    #             # Redirect to login page
-    #             return HttpResponseRedirect(reverse('login'))  # Update 'login' with your actual login URL
-
-        
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.fields['product'].disabled = True
-    #     self.fields['buyer'].disabled = True
