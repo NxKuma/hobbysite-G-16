08dccffa3577a593c218422ba7256b6537a8d9d6
NxKuma <neilisraelko01@gmail.com>
2024-05-06 18:27:30 +0800

    Added Navbar and CSS & JS

--- user_management/urls.py
+++ user_management/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import ProfileUpdateView, Index
+from .views import ProfileUpdateView, index
 
 urlpatterns = [
     path('profile', ProfileUpdateView.as_view(), name='profile-update'),
-    path('', Index.as_view(), name='index'),
+    path('', index, name='index'),
 ]
 
 app_name = 'user_management'
