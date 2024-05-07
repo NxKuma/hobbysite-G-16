a8ed4a3056a08e4a5fbe7952e83b29207611163b
larcomicious <marco.gimena@student.ateneo.edu>
2024-05-07 12:11:46 +0800

    Altered the status fields for Product and Transaction models

--- merchstore/models.py
+++ merchstore/models.py
@@ -11,7 +11,9 @@ class ProductType(models.Model):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('merchstore:product-list', args=[self.pk])
+        return reverse(
+            'merchstore:product-list',
+             args=[self.pk])
 
     class Meta:
         ordering = ['name',]
@@ -32,26 +34,24 @@ class Product(models.Model):
     )
     
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=100)
+    price = models.DecimalField(
+        decimal_places=2, 
+        max_digits=100)
     stock = models.PositiveIntegerField()
-
-    class States(models.TextChoices):
-        # Actual value ↓      # ↓ Displayed on Django Admin
-        AVAILABLE = 'Available', 'Available'
-        ON_SALE = 'On sale', 'On sale'
-        OUT_OF_STOCK = 'Out of stock', 'Out of stock'
-    
     status = models.CharField(
-        max_length = 12,
-        choices = States,
-        default = States.AVAILABLE
-    )
+        max_length=12, 
+        choices=(('Available','Available'), 
+                 ('On sale', 'On sale'), 
+                 ('Out of stock','Out of stock'),),
+                 default='Available')
     
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
-        return reverse('merchstore:product-detail', args=[self.pk])
+        return reverse(
+            'merchstore:product-detail', 
+            args=[self.pk])
 
     class Meta:
         ordering = ['name',]
@@ -70,18 +70,15 @@ class Transaction(models.Model):
         related_name = 'transactions'
     )
     amount = models.PositiveIntegerField()
-
-    class States(models.TextChoices):
-        ONCART = 'On cart', 'On cart'
-        TOPAY = 'To pay', 'To pay'
-        TOSHIP = 'To ship', 'To ship'
-        TORECEIVE = 'To receive', 'To receive'
-        DELIVERED = 'Delivered', 'Delivered'
-    
     status = models.CharField(
-        max_length = 10,
-        choices = States)
-    created_on = models.DateTimeField(auto_now_add=True)
+        max_length=10, 
+        choices=(('On cart','On cart'), 
+                 ('To pay', 'To pay'), 
+                 ('To ship','To ship'), 
+                 ('To receive','To receive'), 
+                 ('Delivered','Delivered'),))
+    created_on = models.DateTimeField(
+        auto_now_add=True)
     
     class Meta:
         ordering = ['-created_on',]
