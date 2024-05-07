bece11bfd1fa863698c49295d1c98d1963d44b77
larcomicious <marco.gimena@student.ateneo.edu>
2024-05-07 12:14:58 +0800

    Applied Zen of Python to format

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -6,12 +6,18 @@ from .views import (
 )
 
 urlpatterns = [
-    path('items', ProductListView.as_view(), name='product-list'),
-    path('item/<int:pk>', ProductDetailView.as_view(), name='product-detail'),
-    path('item/add', ProductCreateView.as_view(), name='product-create'),
-    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name='product-update'),
-    path('cart', CartView.as_view(), name='cart'),
-    path('transactions', TransactionsListView.as_view(), name='transactions'),
+    path('items', ProductListView.as_view(), 
+         name='product-list'),
+    path('item/<int:pk>', ProductDetailView.as_view(), 
+         name='product-detail'),
+    path('item/add', ProductCreateView.as_view(), 
+         name='product-create'),
+    path('item/<int:pk>/edit', ProductUpdateView.as_view(), 
+         name='product-update'),
+    path('cart', CartView.as_view(), 
+         name='cart'),
+    path('transactions', TransactionsListView.as_view(), 
+         name='transactions'),
 ]
 
 app_name = 'merchstore'
